3b9297c70793b3bae6922517380b5f679e45c39a
Robin Böhm <robin.boehm@symetics.com>
2015-09-10 14:04:16 +0200

    feat(dashboard): add global share direct via hoodie

--- www/assets/js/dashboard/dashboard.component.js
+++ www/assets/js/dashboard/dashboard.component.js
@@ -1,30 +1,44 @@
 angular.module('drn.dashboard', ['hoodie'])
   .directive('drnDashboard', function () {
 
-    function drnDashboardVM ($scope, hoodieArray, hoodieAccount) {
+    function drnDashboardVM ($scope, $q) {
 
-      var emptyItem = {urgent:1};
+      var emptyItem = {
+        urgent : 1,
+        by     : hoodie.username
+      };
 
       $scope.items = [];
       $scope.newItem = angular.copy(emptyItem);
 
+
+      function init () {
+        $q.when(hoodie.store.findAll('item'))
+          .then(function (items) {
+            $scope.items = items;
+          });
+      }
+
       $scope.remove = function (item, index) {
+        hoodie.store.find('item', item.id).unpublish();
+        hoodie.store.remove('item', item.id)//.unpublish();
         $scope.items.splice(index, 1);
-        hoodie.store.findAll('item');
       };
 
       $scope.add = function (item) {
         $scope.items.push(item);
         $scope.newItem = angular.copy(emptyItem);
+        hoodie.store.add('item', item).publish();
       };
 
 
       $scope.edit = function (item) {
         $scope.editMode = false;
+        hoodie.store.update('item', item.id, item).publish();
       };
 
-      hoodieArray.bind($scope, 'items', 'item');
-      //hoodie.store.findAll('item').publish();
+
+      init();
     }
 
 
